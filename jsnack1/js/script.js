5552ebac59bdc88b7e8287491cc78fe6edf69e61
IvanRusso91 <iv.russo91@gmail.com>
2022-04-12 15:54:48 +0200

    jsnack1 completato

---
+++ jsnack1/js/script.js
@@ -0,0 +1,30 @@
+
+/*
+ JSnack 1
+ L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+*/
+
+//-------------------------------------------
+
+let primo = prompt("inserisci il primo numero da 1 a 9");
+let secondo = prompt("inserisci il secondo numero 1 a 9");
+let stampa = document.getElementById("box");
+let maggiore;
+let minore;
+
+
+if(primo > secondo){
+
+  stampa.innerHTML = `il numero maggiore è ${primo} e il numero minore è ${secondo}`;
+
+}else if(primo === secondo){
+  stampa.innerHTML=`il risultato è un pareggio`;
+
+}else{
+  maggiore = secondo;
+  minore = primo;
+
+  stampa.innerHTML = `il numero maggiore è ${secondo} e il numero minore è ${primo}`;
+}
+
+
