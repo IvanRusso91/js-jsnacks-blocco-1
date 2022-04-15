98f9e0c2218c7e5607568e198d351e9a4836998e
IvanRusso91 <iv.russo91@gmail.com>
2022-04-15 16:48:23 +0200

    jsnack9 c

---
+++ jsnack9/js/script.js
@@ -0,0 +1,22 @@
+/*
+ JSnack 9
+ Calcola la somma e la media dei primi 10 numeri.
+*/
+
+//-----------------------------------------------------
+
+const stampa = document.getElementById('box');
+let tot = 0;
+
+
+for(let i = 1; i <= 10; i++){
+
+  tot = i + tot;
+
+}
+
+let media = tot / 10;
+
+  console.log(media);
+
+stampa.innerHTML=`la somma delle prime 10 cifre è ${tot}, invece la media è ${media}.`
